cc9b00d275a3f8756b304955de3b6008e5dd7046
Clynton Smith <s5246125@student.ad.griffith.edu.au>
2022-08-26 13:50:06 +1000

    Pause Menu fuckin done

--- Assets/scripts/MenuScripts/DontDestroyOnLoad.cs
+++ Assets/scripts/MenuScripts/DontDestroyOnLoad.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    public GameObject pauseMenu;
     public void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Menu");
-
-        if (objs.Length > 1)
-        {
-            Destroy(this.gameObject);
-        }
-
         DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+        {
+            pauseMenu.SetActive(false);
+        }
     }
 }
