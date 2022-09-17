07ecf2e437c580405f7437c15f2b38b4e4674d76
XeiShare <112142531+XeiShare@users.noreply.github.com>
2022-09-18 01:57:58 +1000

    Improved type script

--- Assets/scripts/GameplayMechanics/Type.cs
+++ Assets/scripts/GameplayMechanics/Type.cs
@@ -7,8 +7,16 @@ public class Type : MonoBehaviour
 {//Put this script on a button.
     
     public Text textBox; //the text object.
+    public GameObject typeSound; //The audio source game object
+    [TextArea(3, 10)]
     public string info; //In this field enter the things you want to be typed on the typewriter. Leave a space at the end.
     public float delay; //The time delay between characters being typed. 0.02 - 0.05 works well.
+    private AudioSource audioSauce;
+
+    void Awake()
+    {
+        audioSauce = typeSound.GetComponent<AudioSource>();
+    }
 
     //Make the on click event this.
     public void EnterLine()
@@ -21,6 +29,10 @@ public class Type : MonoBehaviour
     IEnumerator TypeInfo()
     {
         foreach (char letter in info.ToCharArray()) {
+            if (audioSauce != null)
+            {
+                audioSauce.Play();
+            }
             textBox.text += letter;
             yield return new WaitForSeconds(delay);
         }
