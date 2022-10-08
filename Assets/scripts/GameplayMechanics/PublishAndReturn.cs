c1787f48b34884e1f4ddcdc495be21487fc9bb42
TheGhostStriker <Clynton.Smith@griffithuni.edu.au>
2022-10-08 15:52:10 +1000

    Made new buttons and thingamajigs

---
+++ Assets/scripts/GameplayMechanics/PublishAndReturn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PublishAndReturn : MonoBehaviour
+{
+    public GameObject canvasObject;
+    public GameObject sceneButton;
+    public GameObject typeScene;
+    public GameObject highlightButton;
+    
+
+    public void WhenButtonClicked()
+    {
+        canvasObject.SetActive(true);
+        typeScene.SetActive(false);
+        Destroy(highlightButton);
+        sceneButton.GetComponent<Button>().enabled = false;
+    }
+}
